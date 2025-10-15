5d1a69a8203432a40271308cee5b4ea74d458b16
Sol1de <olivier.delmas@edu.devinci.fr>
2025-10-15 11:11:15 +0200

    :heavy_plus_sign: add `cors` middleware, update TS config, and improve server setup with database connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,23 @@
-import express from "express";
-import type { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from './db.connection';
 
 dotenv.config();
 const app = express();
+const port = process.env.PORT;
 
-const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (request: Request, response: Response) => {
-    response.status(200).send("Hello World");
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log("Server running at PORT: ", PORT);
-}).on("error", (error) => {
-    // gracefully handle error
-    throw new Error(error.message);
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+    });
 });
+
+export default app;
