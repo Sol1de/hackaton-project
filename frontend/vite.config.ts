405803f78715c9ec84a6050d0f1787fa50d92e8d
Sol1de <olivier.delmas@edu.devinci.fr>
2025-10-16 10:37:09 +0200

    :rewind: revert shadcn installation and related changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,8 +8,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
     tailwindcss(),
+    vue(),
     vueDevTools(),
   ],
   resolve: {
