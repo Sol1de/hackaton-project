3ce6f9e770bab047d82f39d43dba76d97f2f7ee4
rdaguet <romain.dagu@gmail.com>
2025-10-16 18:50:47 +0200

    v1 sign in

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,100 +1,22 @@
 <template>
   <div class="flex flex-col md:flex-row h-screen w-full">
     <!-- Left Part -->
-    <div class="flex-1 flex flex-col justify-between items-start p-10 gap-5">
-      <div class="flex flex-col">
+    <div class="flex-1 flex flex-col p-10">
+      <!-- Logo et titres en haut -->
+      <div class="flex flex-col mb-10">
         <img src="./assets/image/logo.png" alt="logo" class="w-15 mb-5" />
         <h1 class="text-5xl font-bold m-0">SOCIAL MEDIA</h1>
         <h2 class="text-lg text-gray-500 m-0 font-normal">Here, everyone is right (especially you).</h2>
       </div>
 
-      <div class="flex flex-col gap-5">
-        <Dialog>
-          <DialogTrigger as-child>
-            <Button variant="outline" class="bg-black text-white">
-              Sign in
-            </Button>
-          </DialogTrigger>
-          <DialogContent class="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Sign in</DialogTitle>
-              <DialogDescription>
-                Sign up here.
-              </DialogDescription>
-            </DialogHeader>
-            <div class="grid gap-4 py-4">
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="firstname" class="text-right">
-                  Firstname
-                </Label>
-                <Input id="firstname" class="col-span-3" />
-              </div>
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="lastname" class="text-right">
-                  Lastname
-                </Label>
-                <Input id="lastname" class="col-span-3" />
-              </div>
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="email" class="text-right">
-                  Email
-                </Label>
-                <Input id="email" class="col-span-3" />
-              </div>
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="password" class="text-right">
-                  Password
-                </Label>
-                <Input id="password" type="password" class="col-span-3" />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button type="submit">
-                Sign in
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-
-        <Dialog>
-          <DialogTrigger as-child>
-            <Button variant="outline">
-              Log in
-            </Button>
-          </DialogTrigger>
-          <DialogContent class="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Log in</DialogTitle>
-              <DialogDescription>
-                Connect you here.
-              </DialogDescription>
-            </DialogHeader>
-            <div class="grid gap-4 py-4">
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="email-login" class="text-right">
-                  Email
-                </Label>
-                <Input id="email-login" class="col-span-3" />
-              </div>
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="password-login" class="text-right">
-                  Password
-                </Label>
-                <Input id="password-login" type="password" class="col-span-3" />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button type="submit">
-                Log in
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+      <!-- Formulaire centré -->
+      <div class="flex-1 flex justify-center items-center">
+        <LoginForm />
       </div>
     </div>
 
-    <!-- Right Part -->
-    <div class="flex-1 flex justify-center items-center">
+    <!-- Right Part (Image) -->
+    <div class="flex-1 hidden md:flex justify-center items-center">
       <img src="./assets/image/Home.png" alt="home" class="w-full h-full object-cover" />
     </div>
   </div>
@@ -113,4 +35,5 @@ import {
 } from '@/components/ui/dialog'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import LoginForm from '@/components/LoginForm.vue'
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Bienvenue sur la page d'accueil</h1>
+  </div>
 </template>
