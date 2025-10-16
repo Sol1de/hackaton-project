73c2146a80293e94c4e3371df3c04e644d951fa5
Sol1de <olivier.delmas@edu.devinci.fr>
2025-10-16 10:23:54 +0200

    :heavy_plus_sign: add utility libraries (`clsx`, `tailwind-merge`, `class-variance-authority`, `lucide-react`), extend TailwindCSS setup, and configure aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,8 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     vue(),
+    tailwindcss(),
     vueDevTools(),
   ],
   resolve: {
