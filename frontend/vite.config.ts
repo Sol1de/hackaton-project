4899dccdb2cc079fc80c50a3b6e489c65cce4952
Sol1de <olivier.delmas@edu.devinci.fr>
2025-10-16 10:46:51 +0200

    :heavy_plus_sign: install `shadcn-vue`, add utility libraries, extend TailwindCSS setup, and configure component aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,8 +8,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     vue(),
+    tailwindcss(),
     vueDevTools(),
   ],
   resolve: {
