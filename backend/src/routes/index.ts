db7c2a8dad35802c3796b7d2891bd2fcc783ad79
riufix <enguerrand.cousin@gmail.com>
2025-10-17 13:35:27 +0200

    mod index.routes for comment routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import express, {Request, Response} from "express"
 import { Router } from "express"
 import { userRouter } from "./user.routes"
 import { postRouter } from "./post.routes"
+import { commentRouter } from "./comment.routes"
 
 export const router = Router()
 
 router.use('/users', userRouter)    
 router.use('/posts', postRouter)
+router.use('/comments', commentRouter)
 
 router.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
