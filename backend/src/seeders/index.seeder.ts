ad86c933d0da9ac01463660f36bf8e2dd2aad077
Sol1de <olivier.delmas@edu.devinci.fr>
2025-10-18 16:01:03 +0200

    :heavy_plus_sign: add `CommentSeeder` for comment creation, update `IndexSeeder` to seed comments for posts

--- backend/src/seeders/index.seeder.ts
+++ backend/src/seeders/index.seeder.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { injectable, container } from 'tsyringe'
 import { UserSeeder } from "./user.seeder"
 import { PostSeeder } from "./post.seeder"
+import { CommentSeeder } from "./comment.seeder"
 import { connectDB, disconnectDB } from "../config/db.connection"
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
@@ -12,7 +13,8 @@ dotenv.config()
 export class IndexSeeder {
     constructor(
         private readonly userSeeder: UserSeeder,
-        private readonly postSeeder: PostSeeder
+        private readonly postSeeder: PostSeeder,
+        private readonly commentSeeder: CommentSeeder
     ) {}
 
     async run() {
@@ -46,26 +48,48 @@ export class IndexSeeder {
                 }
             ]
 
+            const commentTexts = [
+                "Great post! Thanks for sharing.",
+                "Very interesting perspective!",
+                "I completely agree with this.",
+                "This is really helpful, thank you!",
+                "Nice work on this post!",
+                "Fascinating read!",
+                "Thanks for the insights.",
+                "Well written and informative.",
+                "I learned something new today!"
+            ]
+
             const createdUsers = await Promise.all(
                 usersData.map(userData => this.userSeeder.createUser(userData))
             )
 
+            const createdPosts = await Promise.all(
+                createdUsers.flatMap(user =>
+                    Array.from({ length: 3 }, (_, i) =>
+                        this.postSeeder.createPost({
+                            userId: user._id,
+                            title: `Post ${i + 1} of ${user.firstname}`,
+                            content: `This is a content from post ${i + 1} created by ${user.firstname} ${user.lastname}.`
+                        })
+                    )
+                )
+            )
+
             await Promise.all(
-                createdUsers.map(user =>
-                    Promise.all(
-                        Array.from({ length: 3 }, (_, i) =>
-                            this.postSeeder.createPost({
-                                userId: user._id,
-                                title: `Post ${i + 1} of ${user.firstname}`,
-                                content: `This is a content from post ${i + 1} created by ${user.firstname} ${user.lastname}.`
-                            })
-                        )
+                createdUsers.flatMap((user, userIndex) =>
+                    createdPosts.map((post, postIndex) =>
+                        this.commentSeeder.createComment({
+                            content: commentTexts[(userIndex * 3 + postIndex) % commentTexts.length],
+                            userId: user._id,
+                            postId: post._id.toString()
+                        })
                     )
                 )
             )
 
             await disconnectDB()
-            console.log('✅ Seeding completed')
+            console.log('✅ Disconnected from MongoDB')
 
         } catch (error) {
             console.error('❌ Seeding error:', error)
@@ -76,4 +100,4 @@ export class IndexSeeder {
 }
 
 const indexSeeder = container.resolve(IndexSeeder)
-indexSeeder.run()
+indexSeeder.run().then(() => console.log("Seeding complete"))
