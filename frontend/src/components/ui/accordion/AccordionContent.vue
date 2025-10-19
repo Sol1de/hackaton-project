8e14c6f8e3f3a8f8dedf44e94798a4df78436718
Sol1de <olivier.delmas@edu.devinci.fr>
2025-10-19 17:56:14 +0200

    :recycle: refine `Accordion` components animations and class definitions for smoother transitions and improved maintainability

--- frontend/src/components/ui/accordion/AccordionContent.vue
+++ frontend/src/components/ui/accordion/AccordionContent.vue
@@ -14,9 +14,9 @@ const delegatedProps = reactiveOmit(props, "class")
   <AccordionContent
     data-slot="accordion-content"
     v-bind="delegatedProps"
-    class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+    :class="cn('data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm', props.class)"
   >
-    <div :class="cn('pt-0 pb-4', props.class)">
+    <div class="pt-0 pb-4">
       <slot />
     </div>
   </AccordionContent>
